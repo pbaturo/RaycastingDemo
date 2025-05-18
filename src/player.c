bf86be8ff073cac3c2193c20b0eb34f004e28afc
Pawel Baturo <pawel.baturo@example.com>
2025-05-18 13:11:04 +0200

    Add player initialization and movement functions

---
+++ src/player.c
@@ -0,0 +1,23 @@
+#include "../include/game.h"
+
+init_player(t_player *player) {
+    player->x = SCREEN_WIDTH / 2;
+    player->y = SCREEN_HEIGHT / 2;
+}
+
+void move_player(t_player *player, bool up, bool down, bool left, bool right) {
+    int speed = 10;
+    
+    if (up) {
+        player->y -= speed;
+    }
+    if (down) {
+        player->y += speed;
+    }
+    if (left) {
+        player->x -= speed;
+    }
+    if (right) {
+        player->x += speed;
+    }
+}
